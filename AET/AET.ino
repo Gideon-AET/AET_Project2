ad0327e677d288ea6f5a72ec13e17d98920b8374
Taylor Bancroft <taylor.bancroft@austin.utexas.edu>
2022-04-07 14:52:57 -0500

    Created variables and initialized servo and button pins

--- AET/AET.ino
+++ AET/AET.ino
@@ -1,10 +1,21 @@
 /*
-AET Project 2
+AET Shrek Enclsoure
+Team Name: Plantasia
+Team Members: Jessie and Taylor
+Description: button and servo program
 */
+
+//Servo code library
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin =7;
 
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
